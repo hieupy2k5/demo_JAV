b439eb7e9e614ded60af250c950de9bc3efa5cbc
viet2005-68 <23020170@vnu.edu.vn>
2024-10-10 17:09:50 +0700

    Viet commits

--- src/main/java/org/example/demo/LoggedInController.java
+++ src/main/java/org/example/demo/LoggedInController.java
@@ -1,4 +1,14 @@
 package org.example.demo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 public class LoggedInController {
+    @FXML
+    private TextField username;
+
+    @FXML
+    public void cancelButtonClicked(ActionEvent event) {
+        System.exit(0);
+    }
 }
