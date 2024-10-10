eef46436d5511dea747e27b56005c23144709ef6
viet2005-68 <23020170@vnu.edu.vn>
2024-10-10 17:14:33 +0700

    hihi haha 3
    hihi do ngoc

--- src/main/java/org/example/demo/LoggedInController.java
+++ src/main/java/org/example/demo/LoggedInController.java
@@ -2,13 +2,19 @@ package org.example.demo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 public class LoggedInController {
     @FXML
     private TextField username;
 
     @FXML
+    private Button loginButton;
+    
+    @FXML
     public void cancelButtonClicked(ActionEvent event) {
         System.exit(0);
     }
+
+
 }
