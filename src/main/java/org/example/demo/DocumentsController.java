7465d72029cef0018fdbf7ecd13ce333157037a4
Py <hieupy2k5@gmail.com>
2024-10-15 09:09:56 +0700

    Im still here b!tch

---
+++ src/main/java/org/example/demo/DocumentsController.java
@@ -0,0 +1,15 @@
+package org.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DocumentsController extends MenuController {
+
+
+}
